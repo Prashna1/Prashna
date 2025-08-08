09dc44c45f6f33ebac47f96be6a4715f17015c60
Prashna1 <prashnapoudel261@gmail.com>
2025-08-08 11:22:31 +0545

    Add files via upload

---
+++ class2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char name[50] = "Prashna Poude;";
+    int roll_no = 23;
+    float marks = 88.5;
+
+    printf("\n--- Student Information ---\n");
+    printf("Name       : %s\n", name);
+    printf("Roll Number: %d\n", roll_no);
+    printf("Marks      : %.2f\n", marks);
+
+    return 0;
+}
+
+  
+
